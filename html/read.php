3cdc950d45e0dde33d1dc884a393f114eaf38cc5
SnowLeaf <84669392+YukihaYukishiro@users.noreply.github.com>
2024-09-23 12:05:16 +0900

    いったん表示できるようになった (#3)
    ありがちょ

--- html/read.php
+++ html/read.php
@@ -2,39 +2,74 @@
 // レスポンスタイプをapplication/jsonに設定
 header("Content-Type: application/json; charset=UTF-8");
 
-// MariaDB接続情報
-$servername = "mariaDB"; // docker-composeで定義されたサービス名
-$username = "user";      // MariaDBのユーザー名
-$password = "password";  // MariaDBのパスワード
-$dbname = "exampledb";   // MariaDBのデータベース名
 
-// データベース接続を作成
+$servername = "mariaDB"; // docker-composeで定義されたサービス名
+$username = "user"; // MariaDBのユーザー名
+$password = "password"; // MariaDBのパスワード
+$dbname = "exampledb"; // MariaDBのデータベース名
+
+// 接続を作成
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// 接続チェック
+// 接続をチェック
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQLクエリ作成 
-$sql = "SELECT * FROM orders";
+// テーブル名を指定
+$table = "orders";
+
+// カラムとレコードを取得するSQLクエリ(調理中)
+$sql = "SELECT order_id FROM $table where provide_status = 1";
 $result = $conn->query($sql);
 
-// データを取得
-$data = array();
+// レコードを格納する配列
+$records = array();
+
 if ($result->num_rows > 0) {
+    // カラム名を取得
+    $fields = $result->fetch_fields();
+
+    // レコードを出力
     while($row = $result->fetch_assoc()) {
-        $data[] = $row;
+        $record = array();
+        foreach ($fields as $field) {
+            $record[]= $row[$field->name];
+        }
+        $records[] = $record;
     }
-}else{
-    echo "0 results";
 }
 
+// カラムとレコードを取得するSQLクエリ(調理完了)
+$sql = "SELECT order_id FROM $table where provide_status = 2";
+$result = $conn->query($sql);
+
+// レコードを格納する配列
+$records2 = array();
+
+if ($result->num_rows > 0) {
+    // カラム名を取得
+    $fields = $result->fetch_fields();
+
+    // レコードを出力
+    while($row = $result->fetch_assoc()) {
+        $record = array();
+        foreach ($fields as $field) {
+            $record[]= $row[$field->name];
+        }
+        $records2[] = $record;
+    }
+}
+
+$response = array(
+    "cooking" => $records,
+    "completed" => $records2
+);
+
+// レスポンスを出力
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
 
 
-// JSON形式で出力
-echo json_encode($data);
-
-// データベース接続を閉じる
 $conn->close();
+
 ?>
