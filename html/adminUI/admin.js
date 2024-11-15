ec97640254b8b24d3d745adabcf9fb314da3454a
YukihaYukishiro <appletea0514@gmail.com>
2024-11-15 09:22:27 +0900

    商品の追加更新、注文履歴の表示、CSV出力等

---
+++ html/adminUI/admin.js
@@ -0,0 +1,33 @@
+window.onload = init;
+
+
+
+
+function init() {
+    var menu = document.getElementById("top-menu");
+    for (var i = 0; i < menu.children.length; i++) {
+        menu.children[i].addEventListener("click", function() {
+            var href = this.getAttribute("href");
+            var id = href.substring(1);
+            update_dashbord(id);
+        });
+    }
+    var id = window.location.hash.substring(1);
+    if(id != ""){
+        update_dashbord(id);
+    }else{
+        update_dashbord("payment-log");
+    }
+} // end of init
+
+
+
+function update_dashbord(id){
+    var dashbord_contents = document.getElementsByClassName("dashbord-content");
+    for (var i = 0; i < dashbord_contents.length; i++) {
+        dashbord_contents[i].classList.add("hidden");
+    }
+    var dashbord = document.getElementById(id);
+    dashbord.classList.remove("hidden");
+
+}
