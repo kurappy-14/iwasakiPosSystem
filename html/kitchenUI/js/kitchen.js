2dbfe8ccce185c63cc09efcac74df097b2da00c6
hatomato0920 <realwasetake@gmail.com>
2024-11-26 11:45:45 +0900

    :zap: 30+5番のように読み上げるように　再生中にキューに追加されても読み上げるように

--- html/kitchenUI/js/kitchen.js
+++ html/kitchenUI/js/kitchen.js
@@ -19,7 +19,7 @@ function fetchOrders() {
                             <b>注文ID: ${order.order_id}</b>
                             <b>呼び出し番号: ${order.call_number}</b>
                         </div>
-                        <button onclick="updateStatus(${order.order_id}, ${order.provide_status})">次へ</button>
+                        <button onclick="updateStatus(${order.order_id}, ${order.provide_status},${order.call_number})">次へ</button>
                         ${getRevertButton(order.provide_status, order.order_id)}
                     </div>
                     <div class="order-body">
@@ -49,11 +49,12 @@ function fetchOrders() {
 
 // 音声再生用のキュー
 const voicequeue = [];
+const tempvoicequeue = [];
 let voiceWaitingFlag = false; // 音声再生待ちフラグ
 let queueDeletedFlag = false; // キュー削除フラグ
 
 // ステータスを更新する
-async function updateStatus(orderId, currentStatus) {
+async function updateStatus(orderId, currentStatus,call_number) {
     let nextStatus;
 
     // 現在のステータスに応じて次のステータスを決定
@@ -65,13 +66,13 @@ async function updateStatus(orderId, currentStatus) {
         nextStatus = 4; // 「調理中」→「提供待ち」
         
         // 提供待ちになったらその番号の音声を再生
-        if (orderId < 11) { // 今は10番までなら再生
-            voicequeue.push(orderId);
+        if (call_number < 10000) { // 今は9999番までなら再生
+            voicequeue.push(call_number);
             setTimeout(() => {
                 if (voiceWaitingFlag == false) { // フラグが立っていない場合のみ再生
                     voiceWaitingFlag = true; // ここでフラグを立てる
                     console.log(`通常再生開始`);
-                    playZundamonVoice(voicequeue);
+                    playZundamonVoice(voicequeue, tempvoicequeue);
                 } else {
                     // すでに上の行で再生中の場合はreturn
                     return;
@@ -79,6 +80,11 @@ async function updateStatus(orderId, currentStatus) {
             }, 5000); // 5秒待ってから音声再生
         } else {
             return;
+        }
+        // すでに再生中の場合は別のQueueに追加
+        if(voiceWaitingFlag && call_number < 10000) { // &&から先いる？
+            console.log(`再生中なので別のQueueに追加します:${call_number}`);
+            tempvoicequeue.push(call_number);
         }
 
     } else if (currentStatus === 4) {
@@ -97,15 +103,15 @@ async function updateStatus(orderId, currentStatus) {
         });
 }
 
-async function playZundamonVoice(voicequeue) {
+async function playZundamonVoice(voicequeue, tempvoicequeue) {
     const audiolist = [];
-    audiolist.push(`./zundamon/お呼び出しします.wav`);
-    audiolist.push(`./zundamon/注文番号.wav`);
+    audiolist.push(`./zundamon/お呼び出しします.mp3`);
+    audiolist.push(`./zundamon/注文番号.mp3`);
     for (let i = 0; i < voicequeue.length; i++) {
-        audiolist.push(`./zundamon/${voicequeue[i]}番.wav`);
+        makeAudioList(voicequeue, audiolist, i);
     }
-    audiolist.push(`./zundamon/のお客様.wav`);
-    audiolist.push(`./zundamon/お料理が完成いたしました.wav`);
+    audiolist.push(`./zundamon/のお客様.mp3`);
+    audiolist.push(`./zundamon/お料理が完成いたしました.mp3`);
 
     for (let i = 0; i < audiolist.length; i++) {
         const audio = new Audio(audiolist[i]);
@@ -119,7 +125,22 @@ async function playZundamonVoice(voicequeue) {
             console.error(`再生失敗: ${audiolist[i]}`, error);
         }
     }
-    voiceWaitingFlag = false; // 再生が終わったらフラグを戻す
+
+    console.log(`(」・ω・)」ずん!(/・ω・)/だー!`);
+
+    // 再生待ちのQueueがある場合はtempvoicequeueをvoicequeueに移し替えて再生
+    // playZundamonVoiceの再帰呼び出し
+    if(tempvoicequeue.length > 0) {
+        voicequeue = tempvoicequeue;
+        tempvoicequeue = [];
+        console.log(voicequeue);
+        playZundamonVoice(voicequeue, tempvoicequeue);
+    }else if(tempvoicequeue.length == 0) {
+        voiceWaitingFlag = false; // 再生が終わったらフラグを戻す
+    }else {
+        console.log(`これは出ないはずのログだよ！`);
+        return;
+    }
 }
 
 function playAudio(audio) {
@@ -128,6 +149,54 @@ function playAudio(audio) {
             audio.onended = resolve;
         }).catch(reject);
     });
+}
+
+function makeAudioList(voicequeue, audiolist,i) {
+    // voicequeueをコピーそれを割っていく
+    // voicequeueはそのまま残す
+    voicequeueCopy = voicequeue[i];
+    console.log(`Added to makeAudioList: ${voicequeueCopy}`);
+    // 1000以上の場合は`1000の位`をとる
+    while(voicequeueCopy != 0) {
+        if (voicequeueCopy > 1000 && voicequeueCopy % 1000 != 0) {
+            audiolist.push(`zundamon/${Math.floor(voicequeueCopy/1000)*1000}.mp3`);
+            voicequeueCopy = voicequeueCopy % 1000;
+        }
+        // 1000の倍数の場合は`1000の位"番"`をとる
+        if(voicequeueCopy % 1000 == 0) {
+            audiolist.push(`zundamon/${Math.floor(voicequeueCopy/1000)*1000}番.mp3`);
+            voicequeueCopy = voicequeueCopy % 1000;
+            break;
+        }
+        // 1000未満100以上の場合は`100の位`をとる
+        if(voicequeueCopy > 100 && voicequeueCopy % 100 != 0){
+            audiolist.push(`zundamon/${Math.floor(voicequeueCopy/100)*100}.mp3`);
+            voicequeueCopy = voicequeueCopy % 100;
+        }
+        // 100の倍数の場合は`100の位"番"`をとる
+        if(voicequeueCopy % 100 == 0) {
+            audiolist.push(`zundamon/${Math.floor(voicequeueCopy/100)*100}番.mp3`);
+            voicequeueCopy = voicequeueCopy % 100;
+            break;
+        }
+        // 100未満10以上の場合は`10の位`をとる
+        if(voicequeueCopy > 10 && voicequeueCopy % 10 != 0){
+            audiolist.push(`zundamon/${Math.floor(voicequeueCopy/10)*10}.mp3`)
+            voicequeueCopy = voicequeueCopy % 10;
+        }
+        // 10の倍数の場合は`10の位"番"`をとる
+        if(voicequeueCopy % 10 == 0) {
+            audiolist.push(`zundamon/${Math.floor(voicequeueCopy/10)*10}番.mp3`);
+            voicequeueCopy = voicequeueCopy % 10;
+            break;
+        }
+        // 10未満の場合は`1の位"番"`をとる
+        if (voicequeueCopy > 0) {
+            audiolist.push(`zundamon/${voicequeueCopy}番.mp3`);
+            voicequeueCopy = 0;
+            break;
+        }
+    }
 }
 
 // 前のステータスに戻すボタンを作成
